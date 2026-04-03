8ab289f885467baf08124ca28f255321aa0e28ac
rohrmanhyundai <sl.rohrmanhyundai@gmail.com>
2026-04-02 21:17:14 -0400

    add netlify function

---
+++ netlify 4/functions/save-dashboard.js
@@ -0,0 +1,11 @@
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "POST,OPTIONS"
+    },
+    body: JSON.stringify({ message: "Function is working" })
+  };
+};
